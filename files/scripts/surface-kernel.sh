1e533c427a9fb8c3866444403e8bfa53aeef7481
Jackson Linford <jackson@linford.net>
2024-12-28 15:34:01 -0700

    skip first two lines

--- files/scripts/surface-kernel.sh
+++ files/scripts/surface-kernel.sh
@@ -6,7 +6,7 @@ curl -Lo /etc/yum.repos.d/linux-surface.repo \
   https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
 # Remove kmods
-rpm-ostree override remove $(rpm -qa | grep kmod)
+rpm-ostree override remove $(rpm -qa | grep kmod | sed "1,2d")
 
 # Add surface kernel
 rpm-ostree override replace --experimental --from repo='linux-surface' \
