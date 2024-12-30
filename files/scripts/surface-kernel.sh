781243c77a21b1adf113533bf54d91607bdf2724
Jackson Linford <jackson@linford.net>
2024-12-30 13:26:00 -0700

    test for systemctl binary

--- files/scripts/surface-kernel.sh
+++ files/scripts/surface-kernel.sh
@@ -12,4 +12,6 @@ test -f /usr/bin/systemctl.backup && echo "systemctl.backup exists"
 
 # Add surface kernel
 dnf5 -y install --allowerasing kernel-surface kernel-surface-devel libwacom-surface iptsd
+test -f /usr/bin/systemctl && echo "systemctl exists"
+test -f /usr/bin/systemctl.backup && echo "systemctl.backup exists"
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
