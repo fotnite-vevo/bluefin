cd3a1ba0ee9b946a300417a67ee06f4b6ddfb1a4
Jackson Linford <jackson@linford.net>
2024-12-30 11:29:39 -0700

    try using dnf5 for kernel

--- files/scripts/surface-kernel.sh
+++ files/scripts/surface-kernel.sh
@@ -2,17 +2,8 @@
 set -oue pipefail
 
 # Install surface repo
-curl -Lo /etc/yum.repos.d/linux-surface.repo \
-  https://pkg.surfacelinux.com/fedora/linux-surface.repo
-
-# Remove kmods
-rpm-ostree override remove $(rpm -qa | grep kmod | sed "1,2d")
+dnf5 config-manager addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
 # Add surface kernel
-rpm-ostree override replace --experimental --from repo='linux-surface' \
-  kernel-surface \
-  kernel-surface-core \
-  kernel-surface-modules \
-  kernel-surface-modules-core \
-  kernel-surface-modules-extra \
-  kernel-surface-devel
+dnf5 -y remove kernel*
+dnf5 -y install --allowerasing kernel-surface iptsd libwacom-surface
