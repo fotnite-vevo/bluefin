1176ddbc3ef5c0344f84b6b78cedd3a7ff09f647
Jackson Linford <jackson@linford.net>
2024-12-30 14:31:49 -0700

    remove systemctl binary checks

--- files/scripts/surface-kernel.sh
+++ files/scripts/surface-kernel.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-test -f /usr/bin/systemctl && echo "systemctl exists"
-test -f /usr/bin/systemctl.backup && echo "systemctl.backup exists"
-
 # Install surface repo
 dnf5 config-manager addrepo --from-repofile=https://pkg.surfacelinux.com/fedora/linux-surface.repo
 
-test -f /usr/bin/systemctl && echo "systemctl exists"
-test -f /usr/bin/systemctl.backup && echo "systemctl.backup exists"
-
 # Add surface kernel
 dnf5 -y remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-test -f /usr/bin/systemctl && echo "systemctl exists"
-test -f /usr/bin/systemctl.backup && echo "systemctl.backup exists"
 rpm-ostree install kernel-surface kernel-surface-devel
