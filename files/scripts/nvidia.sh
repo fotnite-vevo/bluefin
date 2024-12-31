04d479877cf39f0550248a17b02c5210a52c336a
Jackson Linford <jackson@linford.net>
2024-12-31 15:25:12 -0700

    use rpm-ostree for nvidia

--- files/scripts/nvidia.sh
+++ files/scripts/nvidia.sh
@@ -5,7 +5,7 @@ set -oue pipefail
 dnf5 config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 
 # Install nvidia drivers
-dnf5 -y install nvidia-driver akmod-nvidia
+rpm-ostree install nvidia-driver akmod-nvidia
 
 # Disable nouveau and enable nvidia-drm
 rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
