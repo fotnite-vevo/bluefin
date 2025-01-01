b4975bee6784a218a56a6be08d7364bb2231ed68
Jackson Linford <jackson@linford.net>
2024-12-31 19:14:43 -0700

    use different strategy for building nvidia kmod

--- files/scripts/nvidia.sh
+++ files/scripts/nvidia.sh
@@ -4,8 +4,16 @@ set -oue pipefail
 # Install nvidia repo
 dnf5 config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
 
+# Build nvidia kmod
+dnf5 install akmod-nvidia
+akmods --force
+dnf5 history undo last
+
 # Install nvidia drivers
-rpm-ostree install nvidia-driver akmod-nvidia
+rpm-ostree install /usr/src/akmods/nvidia-kmod.latest nvidia-driver nvidia-driver-cuda
+
+# Disable nvidia repo
+dn5 config-manager setopt fedora-nvidia.enabled=0
 
 # Disable nouveau and enable nvidia-drm
 #rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
